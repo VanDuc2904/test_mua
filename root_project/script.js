a68ed4faa251f333d298a80fa38c59aa31d99a7b
VanDuc2904 <106574320+VanDuc2904@users.noreply.github.com>
2024-09-27 21:37:25 +0700

    Update script.js

--- root_project/script.js
+++ root_project/script.js
@@ -6,8 +6,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+// Add the OpenWeather precipitation layer
+var apiKey = '  var apiKey = '12118cdb333f0039947273d009989237';
+';  // Thay thế bằng OpenWeather API Key của bạn
+L.tileLayer(`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${apiKey}`, {
+  attribution: 'Rainfall data © OpenWeatherMap',
+  opacity: 0.7
+}).addTo(map);
+
 // Fetch and load the GeoJSON data
-  fetch('./your_shapefile.json')
+fetch('./your_shapefile.json')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
